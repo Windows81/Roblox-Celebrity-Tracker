2df230cad2f167f7c7da004c0bf4acdfd05e686e
1630228 <nastyahmede@gmail.com>
2019-04-17 22:37:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const http=require('http');
 const request=require('request');
 const PORT=process.env.PORT||5000;
 
-async function update(){
-	await request.get('https://www.roblox.com/search/users/presence?userIds=2837719',(e1,r1,b1)=>{
+function serverFromHash(id,hash){
+	return new Promise(res=>{
+		var a=[];
+		var m=0;
+		for(var c=0;c<=m;c+=10){
+		}
+	});
+}
+
+function update(){
+	request.get('https://www.roblox.com/search/users/presence?userIds='+2837719,(e1,r1,b1)=>{
 		if(e1)return;
-		if(true)//JSON.parse(b1).PlayerPresences.InGame)
-			request.get('http://www.roblox.com/headshot-thumbnail/image?width=48&height=48&Format=Png&userId=2837719',(e2,r2,b2)=>{
+		if(true)//JSON.parse(b1).PlayerPresences.InGame){
+			var thumb='http://www.roblox.com/headshot-thumbnail/image?width=48&height=48&Format=Png&userId='+2837719;
+			request.get(thumb,async(e2,r2,b2)=>{
 				console.log(r2.request.uri.href);
+				await serverFromHash(606849621,r2.request.uri.href);
 			});
+		}
 	});
 }
 
@@ -21,3 +33,4 @@ const server=http.createServer((req,res)=>{
 server.listen(PORT,()=>{
 	console.log(`Server running on ${PORT}/`);
 });
+update();

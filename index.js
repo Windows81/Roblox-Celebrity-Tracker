73c47c058c9e30cb1689714e11c5225ac2842e58
1630228 <nastyahmede@gmail.com>
2019-04-18 06:27:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,43 @@ const http=require('http');
 const request=require('request');
 const PORT=process.env.PORT||5000;
 
+const places=[
+	//[2837719,606849621],
+	[1630228,2960624866],
+];
+
 function serverFromHash(id,hash){
 	return new Promise(res=>{
 		var a=[];
 		var m=0;
 		for(var c=0;c<=m;c+=10){
+			var url=`https://www.roblox.com/games/getgameinstancesjson?placeId=${id}&startIndex=${c}`;
+			request.get(url,(e,r,b)=>{
+				var t=JSON.parse(b);
+				m=Math.max(m,t.TotalCollectionSize);
+				var srvr=t.Collection.find(v=>{
+					return v.CurrentPlayers.find(v=>
+						{return v.Thumbnail.Url==hash;}
+				});
+				console.log(srvr.Guid);
+			});
 		}
 	});
 }
 
 function update(){
-	request.get('https://www.roblox.com/search/users/presence?userIds='+2837719,(e1,r1,b1)=>{
-		if(e1)return;
-		if(true)//JSON.parse(b1).PlayerPresences.InGame){
-			var thumb='http://www.roblox.com/headshot-thumbnail/image?width=48&height=48&Format=Png&userId='+2837719;
-			request.get(thumb,async(e2,r2,b2)=>{
-				console.log(r2.request.uri.href);
-				await serverFromHash(606849621,r2.request.uri.href);
-			});
-		}
-	});
+	places.forEach(v=>{
+		request.get('https://www.roblox.com/search/users/presence?userIds='+v[1],(e1,r1,b1)=>{
+			if(e1)return;
+			if(JSON.parse(b1).PlayerPresences.InGame){
+				var thumb='http://www.roblox.com/headshot-thumbnail/image?width=48&height=48&Format=Png&userId='+v[1];
+				request.get(thumb,async(e2,r2,b2)=>{
+					console.log(r2.request.uri.href);
+					await serverFromHash(v[2],r2.request.uri.href);
+				});
+			}
+		});
+	}
 }
 
 const server=http.createServer((req,res)=>{
@@ -34,3 +51,4 @@ server.listen(PORT,()=>{
 	console.log(`Server running on ${PORT}/`);
 });
 update();
+setInterval(update,69000);

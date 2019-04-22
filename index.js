e4ed64d97c10a063cdcc5538ff21525368ba19b6
1630228 <nastyahmede@gmail.com>
2019-04-22 04:26:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function serverFromHash(player,place){
 async function update(){
 	places.forEach(v=>{
 		serverFromHash(v[0],v[1]).then(v=>{
-			var url='https://discordapp.com/api/webhooks/568574341764087992/p6VH8vQ-PbEoDmsm1eD6UoXagZniSX7XgO91gkXbDT_4CBGq61qoVV3risqCQkVV2nsV';
+			var url='https://discordapp.com/api/webhooks/568574341764087992'+'/p6VH8vQ-PbEoDmsm1eD6UoXagZniSX7XgO91gkXbDT_4CBGq61qoVV3risqCQkVV2nsV';
 			if(v)request.post({url:url,json:{content:'``'+v+'``'}});
 		});
 	});

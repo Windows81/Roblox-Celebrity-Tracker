69972f58015565f2dfb57dffcefb31a58a28f81b
1630228 <nastyahmede@gmail.com>
2019-04-22 21:06:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,75 @@ const places=[
 	[59967,1224212277],
 ];
 
-function serverFromHash(player,place){
+var hashCache=[];
+function getPlayerHashes(players){
+	return new Promise(res=>{
+		var a=[];players.forEach(p=>{
+			if(hashCache[p]!=null){
+				ret.push([p,hashCache[p]]);
+				if(a.length==players.length)res(a);
+			}else{
+				var thumb='http://www.roblox.com/headshot-thumbnail/image?width=48&height=48&Format=Png&userId='+p;
+				request.get(thumb,(e,r,b)=>{
+					var redir=r.request.uri.href.replace('http','https');
+					a.push([p,hashCache[p]=redir]);
+					if(a.length==players.length)res(a);
+				});
+			}
+		});
+	});
+}
+
+function playersInPlace(players,place){
+	return new Promise(async res=>{
+		var a=[];
+		var m=0;
+		var hashes=await getPlayerHashes(players);
+		
+		for(var c=0;c<=m;c+=7){
+			var url=`https://www.roblox.com/games/getgameinstancesjson?placeId=${place}&startIndex=${c}`;
+			request.get({url:url,headers:{Cookie:'.ROBLOSECURITY='+process.env.roblosecurity}},(e,r,b)=>{
+				var t=JSON.parse(b);
+				m=Math.max(m,t.TotalCollectionSize);
+				t.Collection.forEach(coll=>{
+					coll.CurrentPlayers.forEach(srvPl=>{
+						for(var i=hashes.length-1;i>=0;i--){
+							var hash=hashes[i];
+							if(srvPl.Thumbnail.Url==hash[1]){
+								a.push([hash[0],`Roblox.GameLauncher.joinGameInstance(${place},"${coll.Guid}")`;
+								hashes.splice(i,1);
+							}
+							if(hashes.length==0)res(a);
+						}
+					});
+				});
+			});
+		}
+		res(a);
+	});
+}
+
+async function getPlayersOnline(players){
+	var all=await new Promise(res=>{
+		var a=[];
+		players.forEach(p=>{
+			request.get('https://www.roblox.com/search/users/presence?userIds='+p,(e,r,b)=>{
+				if(!e){
+					var p=JSON.parse(b).PlayerPresences[0];
+					if(p.InGame)a.push([p,p.PlaceId>0?p.FollowToGameScript:null]);
+					if(a.length==players.length)res(a);
+				}
+			});
+		});
+	});
+	var willHash=[];
+	all.forEach(p=>{
+		if(!p[1])willHash.push(p[0]);
+	});
+	
+}
+
+function getCelebrities(players,place){
 	return new Promise(res=>{
 		request.get('https://www.roblox.com/search/users/presence?userIds='+player,(e1,r1,b1)=>{
 			if(!e1&&JSON.parse(b1).PlayerPresences[0].InGame){
